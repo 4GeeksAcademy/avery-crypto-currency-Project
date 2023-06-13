ca319ced27b5a397c4939ff4597c755448402db7
David Avery <124400470+averydi@users.noreply.github.com>
2023-06-13 00:51:56 +0000

    Edits

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,32 @@
-import React from "react";
+import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
 
 export const Navbar = () => {
+	
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 sticky-top">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="logo">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/100px-Star_Wars_Logo.svg.png"></img>	
 			</div>
+			</Link>
+			<div className="dropstart btn-group ml-auto pe-5">
+				<button type="button" className="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					<strong>Favorites UwU</strong>
+				</button>
+				<ul className="dropdown-menu">
+					{store.favorites.map((favorite, index)=>{
+						return(
+							<div className="d-flex flex-row" key={index}>
+								<li key={index}><a className="dropdown-item d-flex flex-row overflow-hidden"  href="#">{favorite.itemName}</a></li>
+								<button onClick={()=>{actions.deleteFavorite(favorite.itemName)}} type="button" className="badge rounded-pill bg-info mx-auto">X</button>
+							</div>
+						)
+					})||<p>How about adding favorites?</p>}
+					<li><a className="dropdown-item" onClick={()=>{actions.deleteAllFavorites()}}>Clear All</a></li>
+				</ul>
+				</div>
 		</nav>
 	);
 };
